2d7c2ebe6f9e8f9fa2661c587372114ff7fe754b
dni <daniele@nicastro.io>
2023-02-09 21:44:38 +0100

    shuffle

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>How To Install Vue 3 in Laravel 9 with Vite</title>
 
-    @vite('resources/css/app.css')
+    @vite('resources/css/app.min.css')
 </head>
 <body>
 <div id="app"></div>
