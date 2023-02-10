745a07c549ee829b4854168a491a550a9fba0c0f
dni <daniele@nicastro.io>
2023-02-10 11:29:55 +0100

    vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@400;500;600;700&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap">
-    @vite('resources/css/app.css','resources/js/app.js')
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 
 </head>
 <body class="antialiased bg-body text-body font-body">
