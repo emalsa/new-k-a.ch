10ec5213b349ef6225ed0bbf9e1b1829519674c0
dni <daniele@nicastro.io>
2023-02-10 11:21:16 +0100

    build

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,13 +10,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@400;500;600;700&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap">
-    @vite('resources/css/tailwind.css')
-<!--    @vite('resources/js/app.js')-->
+    @vite('resources/css/tailwind.css','resources/js/app.js')
 
 </head>
 <body class="antialiased bg-body text-body font-body">
 <div id="app"></div>
 
-@vite('resources/js/app.js')
 </body>
 </html>
