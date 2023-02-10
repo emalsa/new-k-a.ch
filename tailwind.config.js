be60521c844a688d169b53185cf86ad865904cd5
dni <daniele@nicastro.io>
2023-02-10 11:23:31 +0100

    buld 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         './resources/js/components/*.vue',
         "./resources/js/*.js",
         './resources/js/*.vue',
+        './resources/css/*.css',
     ],
 
     theme: {
