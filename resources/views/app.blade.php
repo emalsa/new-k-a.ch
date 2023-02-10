12d722b32c26d561627c97f0bf570dcdc2610729
dni <daniele@nicastro.io>
2023-02-10 23:41:29 +0100

    min.css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,11 +11,17 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@400;500;600;700&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap">
-    @vite('resources/css/app.css')
+  <?php if ($_SERVER['HTTP_HOST'] === 'kirche-austreten.ch' || $_SERVER['HTTP_HOST'] === 'www.kirche-austreten.ch'): ?>
+      @vite('resources/css/min/app.min.css')
+  <?php else: ?>
+      @vite('resources/css/app.css')
+  <?php endif ?>
     @vite('resources/js/app.js')
+
 
 </head>
 <body class="antialiased bg-body text-body font-body">
+<?php echo $_SERVER['HTTP_HOST']; ?>
 <div id="app"></div>
 
 </body>
