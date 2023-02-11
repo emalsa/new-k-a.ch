b413d8d105a965c99607915fade088490aae94b1
dni <daniele@nicastro.io>
2023-02-11 01:07:35 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
+            'resources/css/min/app.min.css'
         ]),
     ],
 });
