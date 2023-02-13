bb37a3adf7475d93095285dddf7ecb871e5448f5
dni <daniele@nicastro.io>
2023-02-13 20:39:42 +0100

    custom purple

--- resources/js/components/Hero.vue
+++ resources/js/components/Hero.vue
@@ -6,15 +6,15 @@
           <div class="max-w-md md:max-w-lg mx-auto xl:mx-0">
             <div class="text-sm mb-2 font-semibold uppercase">
               <span>Develop Your</span>
-              <span class="text-green-500">Skills</span>
+              <span class="text-test-500">Skills</span>
             </div>
             <h1 class="font-heading text-4xl md:text-5xl mb-6">Take care of your performance every day</h1>
             <p class="text-lg leading-8 mb-10">Take care to develop resources continually and integrity them with
               previous projects.</p>
             <div class="sm:flex items-center">
-              <a class="inline-block w-full sm:w-auto py-4 px-6 mb-4 sm:mb-0 sm:mr-4 text-center font-heading font-medium text-base text-white bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600 rounded-sm transition duration-200"
+              <a class="inline-block w-full sm:w-auto py-4 px-6 mb-4 sm:mb-0 sm:mr-4 text-center font-heading font-medium text-base text-white bg-test-500 hover:bg-test-600 border border-test-500 hover:border-test-600 rounded-sm transition duration-200"
                  href="#">Track your
-                performance</a><a class="inline-block w-full sm:w-auto py-4 px-6 text-center font-heading font-medium text-base text-gray-900 hover:text-green-500 border border-gray-900 hover:border-green-500 rounded-sm transition duration-150"
+                performance</a><a class="inline-block w-full sm:w-auto py-4 px-6 text-center font-heading font-medium text-base text-gray-900 hover:text-test-500 border border-gray-900 hover:border-test-500 rounded-sm transition duration-150"
                                   href="#">Learn more</a></div>
           </div>
         </div>
