6cd6d4c5dcc4bf1cfe0a741a19a291e3163b7915
dni <daniele@nicastro.io>
2023-02-13 20:19:06 +0100

    favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,22 +13,22 @@
 
 
 
-    <link rel="apple-touch-icon" sizes="57x57" href="@vite('resources/assets/favicon/apple-icon-57x57.png')">
-    <link rel="apple-touch-icon" sizes="60x60" href="@vite('resources/assets/favicon/apple-icon-60x60.png')">
-    <link rel="apple-touch-icon" sizes="72x72" href="@vite('resources/assets/favicon/apple-icon-72x72.png')">
-    <link rel="apple-touch-icon" sizes="76x76" href="@vite('resources/assets/favicon/apple-icon-76x76.png')">
-    <link rel="apple-touch-icon" sizes="114x114" href="@vite('resources/assets/favicon/apple-icon-114x114.png')">
-    <link rel="apple-touch-icon" sizes="120x120" href="@vite('resources/assets/favicon/apple-icon-120x120.png')">
-    <link rel="apple-touch-icon" sizes="144x144" href="@vite('resources/assets/favicon/apple-icon-144x144.png')">
-    <link rel="apple-touch-icon" sizes="152x152" href="@vite('resources/assets/favicon/apple-icon-152x152.png')">
-    <link rel="apple-touch-icon" sizes="180x180" href="@vite('resources/assets/favicon/favicon-16x16.png')">
-    <link rel="icon" type="image/png" sizes="192x192"  href="@vite('resources/assets/favicon/android-icon-192x192.png')">
-    <link rel="icon" type="image/png" sizes="32x32" href="@vite('resources/assets/favicon/favicon-32x32.png')">
-    <link rel="icon" type="image/png" sizes="96x96" href="@vite('resources/assets/favicon/favicon-96x96.png')">
-    <link rel="icon" type="image/png" sizes="16x16" href="@vite('resources/assets/favicon/favicon-16x16.png')">
-    <link rel="manifest" href="@vite('resources/assets/favicon/manifest.json')">
+    <link rel="apple-touch-icon" sizes="57x57" href={{URL::asset('resources/assets/favicon/apple-icon-57x57.png')}}>
+    <link rel="apple-touch-icon" sizes="60x60" href={{URL::asset('resources/assets/favicon/apple-icon-60x60.png')}}>
+    <link rel="apple-touch-icon" sizes="72x72" href={{URL::asset('resources/assets/favicon/apple-icon-72x72.png')}}>
+    <link rel="apple-touch-icon" sizes="76x76" href={{URL::asset('resources/assets/favicon/apple-icon-76x76.png')}}>
+    <link rel="apple-touch-icon" sizes="114x114" href={{URL::asset('resources/assets/favicon/apple-icon-114x114.png')}}>
+    <link rel="apple-touch-icon" sizes="120x120" href={{URL::asset('resources/assets/favicon/apple-icon-120x120.png')}}>
+    <link rel="apple-touch-icon" sizes="144x144" href={{URL::asset('resources/assets/favicon/apple-icon-144x144.png')}}>
+    <link rel="apple-touch-icon" sizes="152x152" href={{URL::asset('resources/assets/favicon/apple-icon-152x152.png')}}>
+    <link rel="apple-touch-icon" sizes="180x180" href={{URL::asset('resources/assets/favicon/favicon-16x16.png')}}>
+    <link rel="icon" type="image/png" sizes="192x192"  href={{URL::asset('resources/assets/favicon/android-icon-192x192.png')}}>
+    <link rel="icon" type="image/png" sizes="32x32" href={{URL::asset('resources/assets/favicon/favicon-32x32.png')}}>
+    <link rel="icon" type="image/png" sizes="96x96" href={{URL::asset('resources/assets/favicon/favicon-96x96.png')}}>
+    <link rel="icon" type="image/png" sizes="16x16" href={{URL::asset('resources/assets/favicon/favicon-16x16.png')}}>
+    <link rel="manifest" href={{URL::asset('resources/assets/favicon/manifest.json')}}>
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="@vite('resources/assets/favicon/ms-icon-144x144.png')">
+    <meta name="msapplication-TileImage" content={{URL::asset('resources/assets/favicon/ms-icon-144x144.png')}}>
     <meta name="theme-color" content="#ffffff">
 
 
