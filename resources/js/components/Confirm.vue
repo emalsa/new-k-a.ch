0c400b8042271c2ce3481dd132d2cd8de4063ae0
dni <daniele@nicastro.io>
2023-02-14 11:35:51 +0100

    confirm

--- resources/js/components/Confirm.vue
+++ resources/js/components/Confirm.vue
@@ -3,12 +3,14 @@
     <div class="container px-4 mx-auto">
       <div class=" ">
         <div class="mx-auto w-1/2 leading-7">
-          <h3 class="text-2xl text-center mb-8">Es hat geklappt</h3>
-          <p class="mb-2 ">Das Austrittsschreiben wird aktuell generiert.</p>
-          <p class="mb-2 ">Dein Austrittsschreiben sollte in den nächsten 15 Minuten in deiner Mailbox sein. Du wirst
-            noch genaue Instruktionen bekommen in der Email was genau zu tun ist.</p>
+          <h3 class="text-3xl text-center mb-8">Es hat geklappt</h3>
+          <p class="mb-2">Das Austrittsschreiben wird aktuell generiert.</p>
+          <p class="mb-2">Dein Austrittsschreiben sollte in den nächsten 15 Minuten in deiner Mailbox ankommen. Du wirst
+            noch genaue Instruktionen bekommen in der Email was genau zu tun ist mit dem Austrittsschreiben.</p>
           <p> Solltest du keine Email erhalten haben nach 15 Minuten, prüfe ob die Email in den Spamordner gelandet
-            ist. Kontaktiere uns über das Kontaktformular solltest du keine Email erhalten haben.</p>
+            ist. Kontaktiere uns ansonsten über das Kontaktformular, solltest du keine Email erhalten haben.</p>
+
+          <p class="mt-4">Sehr freuen würden wir uns auf dein Feedback.</p>
         </div>
 
       </div>
