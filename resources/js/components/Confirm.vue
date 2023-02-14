77327542842a0498265229907f8ceb575b1e2bbc
dni <daniele@nicastro.io>
2023-02-14 11:22:36 +0100

    confirm

--- resources/js/components/Confirm.vue
+++ resources/js/components/Confirm.vue
@@ -1,9 +1,21 @@
-<template>
-  <h1>Confirm</h1>
+<template xmlns="http://www.w3.org/1999/html">
+  <section class="py-16">
+    <div class="container px-4 mx-auto">
+      <div class=" ">
+        <div class="mx-auto w-1/2 leading-7">
+          <h3 class="text-2xl text-center mb-8">Es hat geklappt</h3>
+          <p class="mb-2 ">Das Austrittsschreiben wird aktuell generiert.</p>
+          <p class="mb-2 ">Dein Austrittsschreiben sollte in den nächsten 15 Minuten in deiner Mailbox sein. Du wirst
+            noch genaue Instruktionen bekommen in der Email was genau zu tun ist.</p>
+          <p> Solltest du keine Email erhalten haben nach 15 Minuten, prüfe ob die Email in den Spamordner gelandet
+            ist. Kontaktiere uns über das Kontaktformular solltest du keine Email erhalten haben.</p>
+        </div>
+
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
