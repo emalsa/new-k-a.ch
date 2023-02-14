96388c1e5cbb305945385aeeba846e748befb914
dni <daniele@nicastro.io>
2023-02-14 13:53:45 +0100

    hero

--- resources/js/components/Hero.vue
+++ resources/js/components/Hero.vue
@@ -9,7 +9,7 @@
               <span class="text-green-500">&nbsp;bestellen</span>
             </div>
             <h1 class="font-heading text-4xl md:text-5xl mb-6">Deinen Kirchenaustritt in der Schweiz</h1>
-            <p class="text-lg leading-8 mb-10">Deinen Kirchenaustritt kannst du gratis in wenigen Minuten erstellen lassen und per Email zukommen lassen. Oder passende Kirchgemeinde finden lassen.</p>
+            <p class="text-lg leading-8 mb-10">Deinen Kirchenaustritt kannst du gratis in wenigen Minuten erstellen und dir per Email zukommen lassen. Oder passende Kirchgemeinde finden lassen.</p>
             <div class="sm:flex items-center">
               <a class="inline-block w-full sm:w-auto py-4 px-6 mb-4 sm:mb-0 sm:mr-4 text-center font-heading font-medium text-base text-white bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600 rounded-sm transition duration-200"
                  href="/#angebot">Kirchenaustritt hier</a><a class="inline-block w-full sm:w-auto py-4 px-6 text-center font-heading font-medium text-base text-gray-900 hover:text-green-500 border border-gray-900 hover:border-green-500 rounded-sm transition duration-150"
