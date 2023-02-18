dc4055186a81a603eb0f4f79b61421cb8b944eab
dni <daniele@nicastro.io>
2023-02-18 11:33:53 +0100

    stepper

--- resources/js/components/Form.vue
+++ resources/js/components/Form.vue
@@ -5,60 +5,62 @@
       <div class="container px-4 mx-auto">
 
         <div class="max-w-3xl mx-auto">
-          <div class="w-full mx-auto mb-14  max-w-2xl">
-            <div class="bg-green-500  h-1 flex items-center justify-between">
 
-              <div class="w-1/3 bg-green-500 h-1 flex items-center">
-                <div class="bg-green-500 h-6 w-6 rounded-full shadow flex items-center justify-center">
-                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/thin_with_steps-svg1.svg" alt="check"/>
-                </div>
-              </div>
+          <!--          Stepper-->
+          <!--          <div class="w-full mx-auto mb-14  max-w-2xl">-->
+          <!--            <div class="bg-green-500  h-1 flex items-center justify-between">-->
 
-              <div class="w-1/3 bg-green-500 h-1 flex items-center">
-                <div class="bg-green-500 h-6 w-6 rounded-full shadow flex items-center justify-center">
-                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/thin_with_steps-svg1.svg" alt="check"/>
-                </div>
-              </div>
+          <!--              <div class="w-1/3 bg-green-500 h-1 flex items-center">-->
+          <!--                <div class="bg-green-500 h-6 w-6 rounded-full shadow flex items-center justify-center">-->
+          <!--                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/thin_with_steps-svg1.svg" alt="check"/>-->
+          <!--                </div>-->
+          <!--              </div>-->
+
+          <!--              <div class="w-1/3 bg-green-500 h-1 flex items-center">-->
+          <!--                <div class="bg-green-500 h-6 w-6 rounded-full shadow flex items-center justify-center">-->
+          <!--                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/thin_with_steps-svg1.svg" alt="check"/>-->
+          <!--                </div>-->
+          <!--              </div>-->
 
 
-              <div class="w-1/3 flex justify-between bg-green-500 h-1 items-center relative">
-                <div class="absolute right-0 -mr-2">
-                  <div class="relative bg-white dark:bg-green-500 shadow-lg px-2 py-1 rounded mt-16 -mr-12">
-                    <svg class="absolute  top-0 -mt-1 w-full right-0 left-0 text-white dark:text-gray-800"
-                         width="16px"
-                         height="8px"
-                         viewBox="0 0 16 8"
-                         version="1.1"
-                         xmlns="http://www.w3.org/2000/svg">
-                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <g id="Progress-Bars" transform="translate(-322.000000, -198.000000)" fill="bg-green-500 ">
-                          <g id="Group-4" transform="translate(310.000000, 198.000000)">
-                            <polygon id="Triangle" points="20 0 28 8 12 8"></polygon>
-                          </g>
-                        </g>
-                      </g>
-                    </svg>
-                    <p tabindex="0" class="focus:outline-none text-white text-xs font-bold">
-                      Step 3: Analyzing</p>
-                  </div>
-                </div>
+          <!--              <div class="w-1/3 flex justify-between bg-green-500 h-1 items-center relative">-->
+          <!--                <div class="absolute right-0 -mr-2">-->
+          <!--                  <div class="relative bg-white dark:bg-green-500 shadow-lg px-2 py-1 rounded mt-16 -mr-12">-->
+          <!--                    <svg class="absolute  top-0 -mt-1 w-full right-0 left-0 text-white dark:text-gray-800"-->
+          <!--                         width="16px"-->
+          <!--                         height="8px"-->
+          <!--                         viewBox="0 0 16 8"-->
+          <!--                         version="1.1"-->
+          <!--                         xmlns="http://www.w3.org/2000/svg">-->
+          <!--                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">-->
+          <!--                        <g id="Progress-Bars" transform="translate(-322.000000, -198.000000)" fill="bg-green-500 ">-->
+          <!--                          <g id="Group-4" transform="translate(310.000000, 198.000000)">-->
+          <!--                            <polygon id="Triangle" points="20 0 28 8 12 8"></polygon>-->
+          <!--                          </g>-->
+          <!--                        </g>-->
+          <!--                      </g>-->
+          <!--                    </svg>-->
+          <!--                    <p tabindex="0" class="focus:outline-none text-white text-xs font-bold">-->
+          <!--                      Step 3: Analyzing</p>-->
+          <!--                  </div>-->
+          <!--                </div>-->
 
-                <div class="bg-green-500 h-6 w-6 rounded-full shadow flex items-center justify-center -ml-2">
-                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/thin_with_steps-svg1.svg" alt="check"/>
-                </div>
+          <!--                <div class="bg-green-500 h-6 w-6 rounded-full shadow flex items-center justify-center -ml-2">-->
+          <!--                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/thin_with_steps-svg1.svg" alt="check"/>-->
+          <!--                </div>-->
 
-                <div class="bg-white dark:bg-green-500 h-6 w-6 rounded-full shadow flex items-center justify-center -mr-3 relative">
-                  <div class="h-3 w-3 bg-green-500  rounded-full"></div>
-                </div>
+          <!--                <div class="bg-white dark:bg-green-500 h-6 w-6 rounded-full shadow flex items-center justify-center -mr-3 relative">-->
+          <!--                  <div class="h-3 w-3 bg-green-500  rounded-full"></div>-->
+          <!--                </div>-->
 
-              </div>
+          <!--              </div>-->
 
-              <div class="w-1/3 flex justify-end">
-                <div class="bg-white dark:bg-green-100 h-6 w-6 rounded-full shadow"></div>
-              </div>
+          <!--              <div class="w-1/3 flex justify-end">-->
+          <!--                <div class="bg-white dark:bg-green-100 h-6 w-6 rounded-full shadow"></div>-->
+          <!--              </div>-->
 
-            </div>
-          </div>
+          <!--            </div>-->
+          <!--          </div>-->
 
           <div class="mb-6 mx-auto max-w-xl">
 
@@ -802,7 +804,7 @@ export default {
         top: 0,
         behavior: "smooth"
       });
-      
+
       e.preventDefault();
     },
     prev(goToStep) {
