b2f58721c511067dcaa4811f43fbb60495cccaad
dni <daniele@nicastro.io>
2023-02-19 13:28:29 +0100

    faq 3

--- resources/js/components/FAQ.vue
+++ resources/js/components/FAQ.vue
@@ -11,7 +11,7 @@
         <ul class="space-y-4">
           <li id="faq-lifetime" class=" px-6 py-8 bg-gray-50 rounded-xl">
             <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">
-              <span class="text-xl font-semibold font-heading">Wie kann ich aus der Kirche austreten?</span>
+              <h3 class="text-xl font-semibold font-heading">Wie kann ich aus der Kirche austreten?</h3>
               <svg class="closed flex-shrink-0"
                    width="20"
                    height="20"
@@ -54,16 +54,18 @@
             <div class="panel">
               <p class="max-w-3xl mt-4 text-base text-gray-700">
                 Für den Austritt aus der Kirche in der Schweiz muss die betreffende Person, welche aus der Kirche
-                austreten will, per Post einen Brief an die zuständige Kirchgemeinde senden. Es gibt kein Online Portal
-                oder ähnliches
-                welche von der Kirche zur Verfügung gestellt wird um einen Kirchenaustritt zu erledigen.
+                austreten will, per Post einen Brief an die zuständige Kirchgemeinde senden.
+              </p>
+              <p class="max-w-3xl mt-4 text-base text-gray-700">
+                Es gibt kein Online Portal, Formular oder ähnliches welche von der Kirche zur Verfügung gestellt wird um
+                einen Kirchenaustritt zu erledigen.
               </p>
             </div>
           </li>
 
           <li class=" px-6 py-8 bg-gray-50 rounded-xl">
             <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">
-              <span class="text-xl font-semibold font-heading">Wohin muss ich mein Austrittsschreiben schicken?</span>
+              <h3 class="text-xl font-semibold font-heading">Wohin muss ich mein Austrittsschreiben schicken?</h3>
               <svg class="closed flex-shrink-0"
                    width="20"
                    height="20"
@@ -112,8 +114,8 @@
               </p>
               <p class="max-w-3xl mt-4 text-base text-gray-700">
                 Die korrekte&nbsp;<a href="#angebot"
-                                     class="underline underline-offset-4 decoration-green-600 hover:decoration-green-400 decoration-[3px]">Adresse
-                der Kirchgemeinde</a> können wir dir geben.
+                                     class="underline underline-offset-4 decoration-green-600 hover:decoration-green-400 decoration-[3px]">
+                Adresse der Kirchgemeinde</a> können wir dir geben.
               </p>
 
             </div>
@@ -121,7 +123,7 @@
 
           <li class=" px-6 py-8 bg-gray-50 rounded-xl">
             <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">
-              <span class="text-xl font-semibold font-heading">Was heisst garantierter Austritt?</span>
+              <h3 class="text-xl font-semibold font-heading">Was heisst <i>Garantierter Austritt</i>?</h3>
               <svg class="closed flex-shrink-0"
                    width="20"
                    height="20"
@@ -162,116 +164,133 @@
               </svg>
             </button>
             <div class="panel">
-              <p class="max-w-3xl mt-4 text-base text-gray-700">Sollten wir beim Austrittsschreiben, den wir für dich
+              <p class="max-w-3xl mt-4 text-base text-gray-700">Bei diesem Angebot musst du nur die notwendigen Daten,
+                also Name, Adresse und Geburtsdatum eingeben. Die Adresse der Kirchgemeinde haben wir. Du musst dein
+                Austrittsschreiben nur ausdrucken, dann unterschreiben und zum Schluss zur Post bringen. Es ist also
+                keine Recherche der Kirchgemeinde notwendig.</p>
+              <p class="max-w-3xl mt-4 text-base text-gray-700">Sollte uns beim Austrittsschreiben, den wir für dich
                 erstellen, einen Fehler unterlaufen sein erstatten wir dir das Geld zurück.
                 Zum Beispiel wenn die falsche Kirchgemeinde im Austrittsschreiben ist. Zudem werden wir dir ein neues
-                Austrittsschreiben mit der korrekten Kirchgemeinde nachschicken per Email.</p>
+                Austrittsschreiben mit der korrekten Kirchgemeinde nachschicken.</p>
             </div>
           </li>
 
-<!--          <li class=" px-6 py-8 bg-gray-50 rounded-xl">-->
-<!--            <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">-->
-<!--              <span class="text-xl font-semibold font-heading">Was passiert nachdem ich das Austrittsschreiben per Post an die Kirchgemeinde geschickt habe?</span>-->
-<!--              <svg class="closed flex-shrink-0"-->
-<!--                   width="20"-->
-<!--                   height="20"-->
-<!--                   viewbox="0 0 20 20"-->
-<!--                   fill="none"-->
-<!--                   xmlns="http://www.w3.org/2000/svg">-->
-<!--                <path fill-rule="evenodd"-->
-<!--                      clip-rule="evenodd"-->
-<!--                      d="M0.750214 10C0.750214 15.108 4.89121 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89121 0.75 0.750214 4.892 0.750214 10Z"-->
-<!--                      stroke="#838EA4"-->
-<!--                      stroke-width="1.5"-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"></path>-->
-<!--                <path d="M6.52902 8.55774L10 12.0437L13.471 8.55774"-->
-<!--                      stroke="#838EA4"-->
-<!--                      stroke-width="1.5"-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"></path>-->
-<!--              </svg>-->
-<!--              <svg class="opened hidden flex-shrink-0"-->
-<!--                   width="20"-->
-<!--                   height="20"-->
-<!--                   viewbox="0 0 20 20"-->
-<!--                   fill="none"-->
-<!--                   xmlns="http://www.w3.org/2000/svg">-->
-<!--                <path fill-rule="evenodd"-->
-<!--                      clip-rule="evenodd"-->
-<!--                      d="M19.2498 10C19.2498 4.892 15.1088 0.75 9.99979 0.75C4.89179 0.75 0.749786 4.892 0.749786 10C0.749786 15.108 4.89179 19.25 9.99979 19.25C15.1088 19.25 19.2498 15.108 19.2498 10Z"-->
-<!--                      stroke="#45C1FF"-->
-<!--                      stroke-width="1.5"-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"></path>-->
-<!--                <path d="M13.471 11.4423L9.99998 7.95626L6.52898 11.4423"-->
-<!--                      stroke="#45C1FF"-->
-<!--                      stroke-width="1.5"-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"></path>-->
-<!--              </svg>-->
-<!--            </button>-->
-<!--            <div class="panel">-->
-<!--              <p class="max-w-3xl mt-4 text-base text-gray-700">We sen</p>-->
-<!--            </div>-->
-<!--          </li>-->
-<!--          -->
-<!--          <li class=" px-6 py-8 bg-gray-50 rounded-xl">-->
-<!--            <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">-->
-<!--              <span class="text-xl font-semibold font-heading">What Actions are Taken After Receiving an Alert? Will You Pursue the Attacker or Thief?</span>-->
-<!--              <svg class="closed flex-shrink-0"-->
-<!--                   width="20"-->
-<!--                   height="20"-->
-<!--                   viewbox="0 0 20 20"-->
-<!--                   fill="none"-->
-<!--                   xmlns="http://www.w3.org/2000/svg">-->
-<!--                <path fill-rule="evenodd"-->
-<!--                      clip-rule="evenodd"-->
-<!--                      d="M0.750214 10C0.750214 15.108 4.89121 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89121 0.75 0.750214 4.892 0.750214 10Z"-->
-<!--                      stroke="#838EA4"-->
-<!--                      stroke-width="1.5"-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"></path>-->
-<!--                <path d="M6.52902 8.55774L10 12.0437L13.471 8.55774"-->
-<!--                      stroke="#838EA4"-->
-<!--                      stroke-width="1.5"-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"></path>-->
-<!--              </svg>-->
-<!--              <svg class="opened hidden flex-shrink-0"-->
-<!--                   width="20"-->
-<!--                   height="20"-->
-<!--                   viewbox="0 0 20 20"-->
-<!--                   fill="none"-->
-<!--                   xmlns="http://www.w3.org/2000/svg">-->
-<!--                <path fill-rule="evenodd"-->
-<!--                      clip-rule="evenodd"-->
-<!--                      d="M19.2498 10C19.2498 4.892 15.1088 0.75 9.99979 0.75C4.89179 0.75 0.749786 4.892 0.749786 10C0.749786 15.108 4.89179 19.25 9.99979 19.25C15.1088 19.25 19.2498 15.108 19.2498 10Z"-->
-<!--                      stroke="#45C1FF"-->
-<!--                      stroke-width="1.5"-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"></path>-->
-<!--                <path d="M13.471 11.4423L9.99998 7.95626L6.52898 11.4423"-->
-<!--                      stroke="#45C1FF"-->
-<!--                      stroke-width="1.5"-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"></path>-->
-<!--              </svg>-->
-<!--            </button>-->
-<!--            <div class="panel">-->
-<!--              <p class="max-w-3xl mt-4 text-base text-gray-700">In the event of unauthorized access, you will be-->
-<!--                promptly notified so that you can take necessary measures such as changing-->
-<!--                your passwords. The alert email will provide information, such as IP address and browser used, about the-->
-<!--                login attempt. Please note that some of this information may not be-->
-<!--                available if the browser blocks it. If necessary, you can forward the information to the authorities for-->
-<!--                further investigation.</p>-->
-<!--            </div>-->
-<!--          </li>-->
+          <li class=" px-6 py-8 bg-gray-50 rounded-xl">
+            <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">
+              <span class="text-xl font-semibold font-heading">Was passiert nachdem ich das Austrittsschreiben an die Kirchgemeinde geschickt habe?</span>
+              <svg class="closed flex-shrink-0"
+                   width="20"
+                   height="20"
+                   viewbox="0 0 20 20"
+                   fill="none"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M0.750214 10C0.750214 15.108 4.89121 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89121 0.75 0.750214 4.892 0.750214 10Z"
+                      stroke="#838EA4"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"></path>
+                <path d="M6.52902 8.55774L10 12.0437L13.471 8.55774"
+                      stroke="#838EA4"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"></path>
+              </svg>
+              <svg class="opened hidden flex-shrink-0"
+                   width="20"
+                   height="20"
+                   viewbox="0 0 20 20"
+                   fill="none"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M19.2498 10C19.2498 4.892 15.1088 0.75 9.99979 0.75C4.89179 0.75 0.749786 4.892 0.749786 10C0.749786 15.108 4.89179 19.25 9.99979 19.25C15.1088 19.25 19.2498 15.108 19.2498 10Z"
+                      stroke="#45C1FF"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"></path>
+                <path d="M13.471 11.4423L9.99998 7.95626L6.52898 11.4423"
+                      stroke="#45C1FF"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"></path>
+              </svg>
+            </button>
+            <div class="panel">
+              <p class="max-w-3xl mt-4 text-base text-gray-700">Du erhältst nach etwa 4-6 Wochen eine Bestätigung per
+                Post von der Kirchgemeinde.
+                Die Kirchgemeinde schickt ebenfalls eine Bestätigung an das Steueramt deiner Gemeinde, um dich von der
+                Kirchensteuer zu befreien.</p>
+              <p class="max-w-3xl mt-4 text-base text-gray-700">Als gute Praxis etabliert hat sich dass das Steueramt
+                deiner Gemeinde von dir ebenfalls über deinen Kirchenaustritt informiert wird.
+                Für den Fall das es die Kirchgemeinde nicht macht.</p>
+              <p class="max-w-3xl mt-4 text-base text-gray-700">
+                Wir schicken dir ebenfalls das Schreiben für das Steueramt beim
+                <a href="#angebot"
+                   class="underline underline-offset-4 decoration-green-600 hover:decoration-green-400 decoration-[3px]">
+                  <i>Garantierter Austritt.</i>
+                </a><br/>Mit der korrekten Anschrift der Gemeinde.
+              </p>
+            </div>
+          </li>
+          <!--          -->
+          <!--          <li class=" px-6 py-8 bg-gray-50 rounded-xl">-->
+          <!--            <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">-->
+          <!--              <span class="text-xl font-semibold font-heading">What Actions are Taken After Receiving an Alert? Will You Pursue the Attacker or Thief?</span>-->
+          <!--              <svg class="closed flex-shrink-0"-->
+          <!--                   width="20"-->
+          <!--                   height="20"-->
+          <!--                   viewbox="0 0 20 20"-->
+          <!--                   fill="none"-->
+          <!--                   xmlns="http://www.w3.org/2000/svg">-->
+          <!--                <path fill-rule="evenodd"-->
+          <!--                      clip-rule="evenodd"-->
+          <!--                      d="M0.750214 10C0.750214 15.108 4.89121 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89121 0.75 0.750214 4.892 0.750214 10Z"-->
+          <!--                      stroke="#838EA4"-->
+          <!--                      stroke-width="1.5"-->
+          <!--                      stroke-linecap="round"-->
+          <!--                      stroke-linejoin="round"></path>-->
+          <!--                <path d="M6.52902 8.55774L10 12.0437L13.471 8.55774"-->
+          <!--                      stroke="#838EA4"-->
+          <!--                      stroke-width="1.5"-->
+          <!--                      stroke-linecap="round"-->
+          <!--                      stroke-linejoin="round"></path>-->
+          <!--              </svg>-->
+          <!--              <svg class="opened hidden flex-shrink-0"-->
+          <!--                   width="20"-->
+          <!--                   height="20"-->
+          <!--                   viewbox="0 0 20 20"-->
+          <!--                   fill="none"-->
+          <!--                   xmlns="http://www.w3.org/2000/svg">-->
+          <!--                <path fill-rule="evenodd"-->
+          <!--                      clip-rule="evenodd"-->
+          <!--                      d="M19.2498 10C19.2498 4.892 15.1088 0.75 9.99979 0.75C4.89179 0.75 0.749786 4.892 0.749786 10C0.749786 15.108 4.89179 19.25 9.99979 19.25C15.1088 19.25 19.2498 15.108 19.2498 10Z"-->
+          <!--                      stroke="#45C1FF"-->
+          <!--                      stroke-width="1.5"-->
+          <!--                      stroke-linecap="round"-->
+          <!--                      stroke-linejoin="round"></path>-->
+          <!--                <path d="M13.471 11.4423L9.99998 7.95626L6.52898 11.4423"-->
+          <!--                      stroke="#45C1FF"-->
+          <!--                      stroke-width="1.5"-->
+          <!--                      stroke-linecap="round"-->
+          <!--                      stroke-linejoin="round"></path>-->
+          <!--              </svg>-->
+          <!--            </button>-->
+          <!--            <div class="panel">-->
+          <!--              <p class="max-w-3xl mt-4 text-base text-gray-700">In the event of unauthorized access, you will be-->
+          <!--                promptly notified so that you can take necessary measures such as changing-->
+          <!--                your passwords. The alert email will provide information, such as IP address and browser used, about the-->
+          <!--                login attempt. Please note that some of this information may not be-->
+          <!--                available if the browser blocks it. If necessary, you can forward the information to the authorities for-->
+          <!--                further investigation.</p>-->
+          <!--            </div>-->
+          <!--          </li>-->
         </ul>
       </div>
       <a class="close-menu group block max-w-max mx-auto font-heading font-medium text-gray-900 hover:text-gray-800 text-base overflow-hidden"
          href="#contact">
-<!--        <p class="mb-1">Didn’t find the answer? Contact us here</p>-->
+        <!--        <p class="mb-1">Didn’t find the answer? Contact us here</p>-->
         <div class="w-full transform -translate-x-0 group-hover:translate-x-full h-px bg-gradient-cyan transition ease-in-out duration-500"></div>
       </a>
     </div>
