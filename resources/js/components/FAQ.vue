db7c5a88a69d262ba1d9e638b89dfdf754bc855b
dni <daniele@nicastro.io>
2023-02-19 13:36:56 +0100

    faq 4

--- resources/js/components/FAQ.vue
+++ resources/js/components/FAQ.vue
@@ -234,58 +234,57 @@
               </p>
             </div>
           </li>
-          <!--          -->
-          <!--          <li class=" px-6 py-8 bg-gray-50 rounded-xl">-->
-          <!--            <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">-->
-          <!--              <span class="text-xl font-semibold font-heading">What Actions are Taken After Receiving an Alert? Will You Pursue the Attacker or Thief?</span>-->
-          <!--              <svg class="closed flex-shrink-0"-->
-          <!--                   width="20"-->
-          <!--                   height="20"-->
-          <!--                   viewbox="0 0 20 20"-->
-          <!--                   fill="none"-->
-          <!--                   xmlns="http://www.w3.org/2000/svg">-->
-          <!--                <path fill-rule="evenodd"-->
-          <!--                      clip-rule="evenodd"-->
-          <!--                      d="M0.750214 10C0.750214 15.108 4.89121 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89121 0.75 0.750214 4.892 0.750214 10Z"-->
-          <!--                      stroke="#838EA4"-->
-          <!--                      stroke-width="1.5"-->
-          <!--                      stroke-linecap="round"-->
-          <!--                      stroke-linejoin="round"></path>-->
-          <!--                <path d="M6.52902 8.55774L10 12.0437L13.471 8.55774"-->
-          <!--                      stroke="#838EA4"-->
-          <!--                      stroke-width="1.5"-->
-          <!--                      stroke-linecap="round"-->
-          <!--                      stroke-linejoin="round"></path>-->
-          <!--              </svg>-->
-          <!--              <svg class="opened hidden flex-shrink-0"-->
-          <!--                   width="20"-->
-          <!--                   height="20"-->
-          <!--                   viewbox="0 0 20 20"-->
-          <!--                   fill="none"-->
-          <!--                   xmlns="http://www.w3.org/2000/svg">-->
-          <!--                <path fill-rule="evenodd"-->
-          <!--                      clip-rule="evenodd"-->
-          <!--                      d="M19.2498 10C19.2498 4.892 15.1088 0.75 9.99979 0.75C4.89179 0.75 0.749786 4.892 0.749786 10C0.749786 15.108 4.89179 19.25 9.99979 19.25C15.1088 19.25 19.2498 15.108 19.2498 10Z"-->
-          <!--                      stroke="#45C1FF"-->
-          <!--                      stroke-width="1.5"-->
-          <!--                      stroke-linecap="round"-->
-          <!--                      stroke-linejoin="round"></path>-->
-          <!--                <path d="M13.471 11.4423L9.99998 7.95626L6.52898 11.4423"-->
-          <!--                      stroke="#45C1FF"-->
-          <!--                      stroke-width="1.5"-->
-          <!--                      stroke-linecap="round"-->
-          <!--                      stroke-linejoin="round"></path>-->
-          <!--              </svg>-->
-          <!--            </button>-->
-          <!--            <div class="panel">-->
-          <!--              <p class="max-w-3xl mt-4 text-base text-gray-700">In the event of unauthorized access, you will be-->
-          <!--                promptly notified so that you can take necessary measures such as changing-->
-          <!--                your passwords. The alert email will provide information, such as IP address and browser used, about the-->
-          <!--                login attempt. Please note that some of this information may not be-->
-          <!--                available if the browser blocks it. If necessary, you can forward the information to the authorities for-->
-          <!--                further investigation.</p>-->
-          <!--            </div>-->
-          <!--          </li>-->
+
+          <li class=" px-6 py-8 bg-gray-50 rounded-xl">
+            <button class="accordion w-full flex justify-between items-center text-left font-semibold font-heading">
+              <span class="text-xl font-semibold font-heading">Muss ich noch ein Gespräch führen wenn ich Austreten will?</span>
+              <svg class="closed flex-shrink-0"
+                   width="20"
+                   height="20"
+                   viewbox="0 0 20 20"
+                   fill="none"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M0.750214 10C0.750214 15.108 4.89121 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89121 0.75 0.750214 4.892 0.750214 10Z"
+                      stroke="#838EA4"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"></path>
+                <path d="M6.52902 8.55774L10 12.0437L13.471 8.55774"
+                      stroke="#838EA4"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"></path>
+              </svg>
+              <svg class="opened hidden flex-shrink-0"
+                   width="20"
+                   height="20"
+                   viewbox="0 0 20 20"
+                   fill="none"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M19.2498 10C19.2498 4.892 15.1088 0.75 9.99979 0.75C4.89179 0.75 0.749786 4.892 0.749786 10C0.749786 15.108 4.89179 19.25 9.99979 19.25C15.1088 19.25 19.2498 15.108 19.2498 10Z"
+                      stroke="#45C1FF"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"></path>
+                <path d="M13.471 11.4423L9.99998 7.95626L6.52898 11.4423"
+                      stroke="#45C1FF"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"></path>
+              </svg>
+            </button>
+            <div class="panel">
+              <p class="max-w-3xl mt-4 text-base text-gray-700">Grundsätzlich Nein.</p>
+              <p class="max-w-3xl mt-4 text-base text-gray-700">In ganz wenigen Gemeinden ist der Austritt mit einem
+                Gespräch verbunden. Die Anzahl dieser Gemeinden ist jedoch zu vernachlässigen. Das haben die
+                entsprechenden Landeskirchen gesetzlich durchgesetzt.</p>
+            </div>
+          </li>
+
         </ul>
       </div>
       <a class="close-menu group block max-w-max mx-auto font-heading font-medium text-gray-900 hover:text-gray-800 text-base overflow-hidden"
