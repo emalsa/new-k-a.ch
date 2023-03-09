efc9356e538279597b3ded7ef1ea7f477f266f96
dni <daniele@nicastro.io>
2023-03-09 11:00:28 +0100

    send

--- app/Jobs/SendPdfJob.php
+++ app/Jobs/SendPdfJob.php
@@ -100,7 +100,6 @@ class SendPdfJob implements ShouldQueue {
    * @return void
    */
   public function handle() {
-    return;
     try {
       $apiKey = env('MAILGUN_APIKEY');
       $configurator = new HttpClientConfigurator();
