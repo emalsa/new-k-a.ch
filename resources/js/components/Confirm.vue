c967354a79742073387f320d80bbb6fe8da01e88
Daniele <s@f.com>
2023-09-12 20:09:41 +0200

    add confirmation page

--- resources/js/components/Confirm.vue
+++ resources/js/components/Confirm.vue
@@ -5,13 +5,14 @@
                 <div class="mx-auto w-1/2 leading-7">
                     <h3 class="text-3xl text-center mb-8">Vielen Dank!</h3>
                     <p class="mb-2">Das Austrittsschreiben wird aktuell erstellt und sollte nächstens in deiner Mailbox
-                        ankommen. Du wirst
-                        in der Email noch genaue Instruktionen bekommen was genau zu tun ist.</p>
+                        ankommen. Du wirst in der Email noch genauere Instruktionen bekommen was zu tun ist.</p>
                     <p>Solltest du nach etwa 2 Stunden keine Email erhalten, prüfe ob unsere Email in deinen Spamordner
                         gelandet ist.</p>
 
                     <p class="mt-4">Wir würden uns über ein Feedback freuen. Du kannst uns einfach über das <a
-                        href="https://www.kirche-austreten.ch/#kontakt">Kontaktformular</a>kontaktieren</p>
+                        class="underline underline-offset-4 decoration-green-600 hover:decoration-green-400 decoration-[3px]"
+                        href="https://www.kirche-austreten.ch/#kontakt">Kontaktformular</a> kontaktieren</p>
+
                 </div>
 
             </div>
