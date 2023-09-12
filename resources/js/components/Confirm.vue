9d3ae65a6271bd422b8d757115e4213916d691b8
Daniele <s@f.com>
2023-09-12 20:07:34 +0200

    add confirmation page

--- resources/js/components/Confirm.vue
+++ resources/js/components/Confirm.vue
@@ -1,20 +1,22 @@
 <template xmlns="http://www.w3.org/1999/html">
-  <section class="py-16">
-    <div class="container px-4 mx-auto">
-      <div class=" ">
-        <div class="mx-auto w-1/2 leading-7">
-          <h3 class="text-3xl text-center mb-8">Es hat geklappt</h3>
-          <p class="mb-2">Das Austrittsschreiben wird aktuell erstellt und sollte in den nächsten 15 Minuten in deiner Mailbox ankommen. Du wirst
-            in der Email noch genaue Instruktionen bekommen was genau zu tun ist.</p>
-          <p> Solltest du keine Email erhalten haben nach 15 Minuten, prüfe ob die Email in den Spamordner gelandet
-            ist.</p>
+    <section class="py-16">
+        <div class="container px-4 mx-auto">
+            <div class=" ">
+                <div class="mx-auto w-1/2 leading-7">
+                    <h3 class="text-3xl text-center mb-8">Vielen Dank!</h3>
+                    <p class="mb-2">Das Austrittsschreiben wird aktuell erstellt und sollte nächstens in deiner Mailbox
+                        ankommen. Du wirst
+                        in der Email noch genaue Instruktionen bekommen was genau zu tun ist.</p>
+                    <p>Solltest du nach etwa 2 Stunden keine Email erhalten, prüfe ob unsere Email in deinen Spamordner
+                        gelandet ist.</p>
 
-          <p class="mt-4">Wir freuen uns über dein Feedback.</p>
+                    <p class="mt-4">Wir würden uns über ein Feedback freuen. Du kannst uns einfach über das <a
+                        href="https://www.kirche-austreten.ch/#kontakt">Kontaktformular</a>kontaktieren</p>
+                </div>
+
+            </div>
         </div>
-
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
